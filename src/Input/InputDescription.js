70dfe34f4ebd555e1e585209c25ca50e1bedc9c9
rrosztoczy <rrosztoczy@gmail.com>
2019-07-21 14:12:36 -0600

    created controlled form with hooks

--- src/Input/InputDescription.js
+++ src/Input/InputDescription.js
@@ -1,49 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form } from 'semantic-ui-react'
 
 function InputDescription() {
 
     const options = [
+        { key: 'u', text: 'Unsure', value: 'unsure' },
         { key: 'm', text: 'Male', value: 'male' },
         { key: 'f', text: 'Female', value: 'female' },
         { key: 'o', text: 'Other', value: 'other' },
-      ]
-      
-    let state = {}
-    let { value } = state
-    let handleChange = (e, { value }) => {}
-    // let handleChange = (e, { value }) => setState({ value })
+      ];
+    const [goal, setGoal] = useState(options[0].text);  
+    const [goalSummary, setGoalSummary] = useState("");  
+    const [goalDescription, setGoalDescription] = useState("");  
+
+    console.log("goal state is", goal)
+    console.log("goalSum state is", goalSummary)
+    console.log("goalDes state is", goalDescription)
 
 
     return (
             <Form>
-              {/* <Form.Group widths='equal'> */}
-                <Form.Select fluid label='Current Goal' options={options} placeholder='Unsure' />
-                <Form.Input fluid label='Specifically, what are you trying to accomplish?' placeholder='I want to...' />
-              {/* </Form.Group> */}
-              <Form.Group inline>
-                <label>Size</label>
-                <Form.Radio
-                  label='Small'
-                  value='sm'
-                  checked={value === 'sm'}
-                  onChange={handleChange}
-                />
-                <Form.Radio
-                  label='Medium'
-                  value='md'
-                  checked={value === 'md'}
-                  onChange={handleChange}
-                />
-                <Form.Radio
-                  label='Large'
-                  value='lg'
-                  checked={value === 'lg'}
-                  onChange={handleChange}
-                />
-              </Form.Group>
-              <Form.TextArea label='About' placeholder='Tell us more about you...' />
-              <Form.Checkbox label='I agree to the Terms and Conditions' />
+              <Form.Select value={goal} onChange={e => setGoal(e.target.innerText)}  options={options} fluid label='Current Goal' placeholder='Select Goal'/>
+              <Form.Input value={goalSummary} onChange={e => setGoalSummary(e.target.value)}  fluid label='Specifically, what are you trying to accomplish?' placeholder='I want to...' />
+              <Form.TextArea value={goalDescription} onChange={e => setGoalDescription(e.target.value)} label='Description' />
               <Form.Button>Submit</Form.Button>
             </Form>
           )
--- src/Output/OutputList.js
+++ src/Output/OutputList.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react';
 import OutputItem from './OutputItem'
 
 function OutputList() {
+    // initial goal: take input from form, break into array and print out list of what was typed
     const renderOutputList = () => {
         return <OutputItem/>
     }
